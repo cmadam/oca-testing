b549ef12d101da9f551fa3baa39ed1a1f308d27e
Constantin Adam <cmadam@us.ibm.com>
2023-03-06 21:23:37 -0500

    Create custom script for local creation of venv

--- create-venv-local.sh
+++ create-venv-local.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/bash -x
 CRT_DIR=${PWD}
 mkdir -p ${HOME}/huntingtest
 cd ${HOME}/huntingtest
-./create-venv.sh
+pip install --upgrade pip
+virtualenv -p python3 huntingtest
+echo "NB: This script created a virtual environment, but did not activate it."
+echo "To activate, run \"source ${HOME}/huntingtest/huntingtest/bin/activate\""
 cd ${CRT_DIR}
