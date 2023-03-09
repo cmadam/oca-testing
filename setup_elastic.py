a99a7fd6adddcd3b64deafbaf6e49b67e45d3845
Constantin Adam <cmadam@us.ibm.com>
2023-03-09 11:18:52 -0500

    Scrub password from elastic log

--- setup_elastic.py
+++ setup_elastic.py
@@ -1,7 +1,10 @@
+import base64
+import os
 import requests
 import shlex
 import subprocess
 import sys
+import time
 
 print("Checking existence of the 'elastic' network")
 proc1 = subprocess.run(shlex.split('docker network ls'), capture_output=True, encoding='utf-8')
@@ -36,24 +39,59 @@ proc3 = subprocess.run(
 if proc3.returncode != 0:
     print(f"Failed to create elasticsearch docker instance 'es01': "
           f"error code {proc3.returncode},\n{proc3.stderr}")
-    # sys.exit(-1)
-
-proc4 = subprocess.run(shlex.split('docker logs es01'), capture_output=True,
-                       encoding='utf-8')
-if proc4.returncode != 0:
-    print(f"Failed to get logs for 'es01': error code {proc3.returncode}\n"
-          f"{proc3.stderr}")
     sys.exit(-1)
-pwd_string = "Password for the"
-output_lines = proc4.stdout.split('\n')
+
 pwd_found = False
-for ix, line in enumerate(output_lines):
-    if pwd_string in line:
-        pwd_found = True
-        break
-print(f"ix = {ix}")
+num_attempts = 1
+while not pwd_found and num_attempts < 11:
+    print(f"Attempt {num_attempts} to find password ...")
+    proc4 = subprocess.run(shlex.split('docker logs es01'), capture_output=True,
+                        encoding='utf-8')
+    if proc4.returncode != 0:
+        print(f"Failed to get logs for 'es01': error code {proc3.returncode}\n"
+            f"{proc3.stderr}")
+        sys.exit(-1)
+    pwd_string = "Password for the"
+    output_lines = proc4.stdout.split('\n')
+    for ix, line in enumerate(output_lines):
+        if pwd_string in line:
+            pwd_found = True
+            break
+    time.sleep(5)
+    num_attempts += 1
 if not pwd_found:
-    print("Could not find password in the logs")
+    print(f"Could not find password in the logs after {num_attempts} tries")
+    sys.exit(-1)
 else:
-    pwd = output_lines[ix + 1].strip()
-    print(f"The password is: {pwd}")
+    log_pwd = str(output_lines[ix + 1].encode('utf-8', errors='ignore').strip())
+    tokens = log_pwd.split('\\x1b[')
+    print(tokens)
+    pwd = tokens[1][2:]
+pwd_file_name = os.path.join(os.getenv('HOME'), '.es_pwd')
+with open(pwd_file_name, 'w') as f:
+    f.write(pwd)
+
+elastic_ready = False
+localhost_url = 'https://localhost:9200'
+attempts = 1
+headers = {}
+headers['Authorization'] = b"Basic " + base64.b64encode(f'elastic:{pwd}'.encode('ascii'))
+while not elastic_ready and attempts <= 20:
+    try:
+        res = requests.get(localhost_url, headers=headers, verify=False)
+        print(f"{res.status_code}: {res.text}")
+        if res.status_code == 200:
+            elastic_ready = True
+        else:
+            print(f"Attempt {attempts} to ping elastic server")
+            attempts += 1
+            time.sleep(5)
+    except:
+        print(f"Attempt {attempts} to ping elastic server after failing to connect")
+        attempts += 1
+        time.sleep(5)
+if elastic_ready:
+    print(f"Elastic server ready after {attempts} attempts")
+else:
+    print(f"Gave up after {attempts} attempts")
+    sys.exit(-1)
