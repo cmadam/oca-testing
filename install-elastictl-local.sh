9f5c1b3e5375c9791d4d91d8b3d79f4ff4c71a72
Constantin Adam <cmadam@us.ibm.com>
2023-03-13 16:44:43 -0400

    Remove elastictl installation scripts

--- install-elastictl-local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CRT_DIR=${PWD}
-source ${HOME}/huntingtest/huntingtest/bin/activate
-cd ${HOME}/huntingtest
-cp ${CRT_DIR}/elastictl-patch/import.go elastictl/tools/
-cp ${CRT_DIR}/Dockerfile.build.elastictl elastictl
-${CRT_DIR}/install-elastictl.sh
-cd ${CRT_DIR}
--- install-elastictl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd elastictl
-export DOCKER_BUILDKIT=1
-docker build --target bin --output bin/ -f Dockerfile.build.elastictl .
-bin/elastictl e --help
