4dd0a9c24d2eb122963d56b4f9f6248f55833c4b
Constantin Adam <cmadam@us.ibm.com>
2023-03-14 13:15:16 -0400

    install requests

--- install-elastic.sh
+++ install-elastic.sh
@@ -6,7 +6,7 @@ echo "Created 'elastic' docker network"
 
 echo "docker run -d --name es01 -e ES_JAVA_OPTS=\"-Xms1g -Xmx1g\" -e \"discovery.type=single-node\" --net elastic -p 9200:9200 -it docker.elastic.co/elasticsearch/elasticsearch:8.6.2"
 docker run -d --name es01 -e ES_JAVA_OPTS="-Xms1g -Xmx1g" -e "discovery.type=single-node" --net elastic -p 9200:9200 -it docker.elastic.co/elasticsearch/elasticsearch:8.6.2
-
+pip install requests
 python setup_elastic.py
 
 echo "docker exec -it es01 bash -c \"bin/elasticsearch-reset-password -u elastic -s -b\""
