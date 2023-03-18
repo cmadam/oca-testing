bdeead14ff29b73014789877b96d186a96e17fda
Constantin Adam <cmadam@us.ibm.com>
2023-03-17 22:52:26 -0400

    Update the repos/branches to clone

--- checkout-code.sh
+++ checkout-code.sh
@@ -2,6 +2,6 @@
 CRT_DIR="${PWD}"
 mkdir -p "${HOME}"/huntingtest
 cd "${HOME}"/huntingtest || exit
-git clone git@github.com:cmadam/stix-shifter.git
-git clone git@github.com:cmadam/kestrel-lang.git
+git clone git@github.com:opencybersecurityalliance/stix-shifter.git
+git clone -b develop_stixshifter_v5 git@github.com:opencybersecurityalliance/kestrel-lang.git
 cd "${CRT_DIR}" || exit
