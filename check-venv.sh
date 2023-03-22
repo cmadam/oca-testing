1d05d4efbf177489b7dac0198e67a7b4c2e4620f
Constantin Adam <cmadam@us.ibm.com>
2023-03-21 22:10:45 -0400

    Run only in ${HOME}/huntingtest/huntingtest venv

--- check-venv.sh
+++ check-venv.sh
@@ -7,4 +7,13 @@ then
     echo "  1. Run 'make venv' to create a virtual environment"
     echo "  2. Run 'source ${HOME}/huntingtest/huntingtest/bin/activate' to activate virtual environment"
     exit 1
+else
+    if [[ "$VIRTUAL_ENV" != "${HOME}/huntingtest/huntingtest" ]]
+    then
+        echo "Current venv (${VIRTUAL_ENV}) is not huntingtest, will not proceed with installation"
+        echo "To install and activate the huntingtest testing environment:"
+        echo "  1. Run 'make venv' to create the huntingtest virtual environment"
+        echo "  2. Run 'source ${HOME}/huntingtest/huntingtest/bin/activate' to activate virtual environment"
+        exit 1
+    fi
 fi
