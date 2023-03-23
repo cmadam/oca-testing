7b3dba0da02a0720e61bf98d88868a86a33d6afe
Constantin Adam <cmadam@us.ibm.com>
2023-03-23 14:32:34 -0400

    Reduce logging output for failing scenarios

--- run-bdd-tests.sh
+++ run-bdd-tests.sh
@@ -5,4 +5,4 @@ ES_PWD=$(cat "${HOME}"/.es_pwd)
 cp kestrel-stixshifter-config.yml "${HOME}"/kestrel-stixshifter-config.yaml
 sed -i "s/<ES_PWD>/${ES_PWD}/" "${HOME}"/kestrel-stixshifter-config.yaml
 export KESTREL_STIXSHIFTER_CONFIG=${HOME}/kestrel-stixshifter-config.yaml
-behave
+behave --logging-level CRITICAL
