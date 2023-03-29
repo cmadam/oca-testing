e2b82a216e37cab618a6126a34784b1f298474ae
Constantin Adam <cmadam@us.ibm.com>
2023-03-29 10:06:22 -0400

    Use port 9234 for elasticsearch test instance

--- install-elastic.sh
+++ install-elastic.sh
@@ -4,8 +4,8 @@ echo "docker network create elastic"
 docker network create elastic
 echo "Created 'elastic' docker network"
 
-echo "docker run -d --name es01 -e ES_JAVA_OPTS=\"-Xms1g -Xmx1g\" -e \"discovery.type=single-node\" --net elastic -p 9200:9200 -it docker.elastic.co/elasticsearch/elasticsearch:8.6.2"
-docker run -d --name es01 -e ES_JAVA_OPTS="-Xms1g -Xmx1g" -e "discovery.type=single-node" --net elastic -p 9200:9200 -it docker.elastic.co/elasticsearch/elasticsearch:8.6.2
+echo "docker run -d --name es01 -e ES_JAVA_OPTS=\"-Xms1g -Xmx1g\" -e \"discovery.type=single-node\" --net elastic -p 9234:9200 -it docker.elastic.co/elasticsearch/elasticsearch:8.6.2"
+docker run -d --name es01 -e ES_JAVA_OPTS="-Xms1g -Xmx1g" -e "discovery.type=single-node" --net elastic -p 9234:9200 -it docker.elastic.co/elasticsearch/elasticsearch:8.6.2
 pip install requests
 python setup_elastic.py
 
